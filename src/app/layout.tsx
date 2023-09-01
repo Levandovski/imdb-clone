33e0f8f5be2784ebf30f07e2fb2e843f6dd8d27a
Jessé Levandovski <jesse.levandovski@outlook.com>
2023-09-01 10:32:03 -0300

    create the header sectio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Header from '@/components/Header';
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'IMDb Clone',
   description: 'This is the IMDb clone website',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">   
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body>
+        <Header />
+        {/* Navbar */}
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
-  )
+  );
 }
